b932754585f6ad48f77643a716b136ad48b9b816
devburakfiliz <dev.burakfiliz@gmail.com>
2022-08-03 14:51:27 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 
 import { useEffect, useState } from 'react';
-import { FormControl, Button} from 'react-bootstrap';
+import { FormControl, Button, Form, FormCheck } from 'react-bootstrap';
 
 
 function App() {
-  const [todoList, setTodoList]= useState([])
-  const [todo,setTodo]= useState('')
-  const addTodo=()=>
-  {
+  const [todoList, setTodoList] = useState([])
+  const [todo, setTodo] = useState('')
+  const addTodo = () => {
     console.log(todo)
-    setTodoList(prevTodoList=> [...prevTodoList, todo])
+    setTodoList(prevTodoList => [...prevTodoList, todo])
     setTodo('')
   }
-  useEffect(() => {console.log (todoList) } , [todoList])
+
   return (
     <div className="d-flex flex-column justify-content-center align-items-center mt-5">
       <h1 className="mt-5">Todo List</h1>
@@ -21,25 +20,45 @@ function App() {
           className="w-75 mr-4"
           placeholder="Todo Input"
           value={todo}
-          onChange={(e)=> setTodo(e.target.value)}
-          
-        
+          onChange={(e) => setTodo(e.target.value)}
+
+
         />
-        <Button className="ms-5" onClick={()=>addTodo()}>Add Todo</Button>
+        <Button className="ms-5" onClick={() => addTodo()}>Add Todo</Button>
       </div>
 
-    <div className='mt-5'>
-
-      {
-        todoList.map(
-          (todoItem, index) => <div key={index}>{todoItem}</div>
+      <div className='mt-5 w-75'>
+        {
+          todoList.map(
+            (todoItem,index)=>
+              <div key={index}className="d-flex justify-content-between">
+                <div className='d-flex'>
+                  <Form.Check
+                  type='checkbox'
+                  className='me-2 d-flex'
+                  />
+                  <label>
+                    {todoItem}
+                  </label>
+                </div>
+                <div>
+                  Butonlar
+                </div>
+              </div>
           )
-      }
-    </div>
+
+        }
+
+
+
+
+
+
+        
+      </div>
     </div>
   );
 }
 
 
 export default App;
-  
