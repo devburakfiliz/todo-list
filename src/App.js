4b53e4aa6e082b6a6d87f53941c58294cc0e89d9
devburakfiliz <dev.burakfiliz@gmail.com>
2022-08-03 13:43:55 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect, useState } from 'react';
+import { FormControl, Button} from 'react-bootstrap';
+
 
 function App() {
+  const [todoList, setTodoList]= useState([])
+  const [todo,setTodo]= useState('')
+  const addTodo=()=>
+  {
+    console.log(todo)
+    setTodoList(prevTodoList=> [...prevTodoList, todo])
+    setTodo('')
+  }
+  useEffect(() => {console.log (todoList) } , [todoList])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello</h1>
-      </header>
+    <div className="d-flex flex-column justify-content-center align-items-center mt-5">
+      <h1 className="mt-5">Todo List</h1>
+      <div className="d-flex w-50">
+        <FormControl
+          className="w-75 mr-4"
+          placeholder="Todo Input"
+          value={todo}
+          onChange={(e)=> setTodo(e.target.value)}
+          
+        
+        />
+        <Button className="ms-5" onClick={()=>addTodo()}>Add Todo</Button>
+      </div>
+
+    <div className='mt-5'>
+
+      {
+        todoList.map(
+          (todoItem, index) => <div key={index}>{todoItem}</div>
+          )
+      }
+    </div>
     </div>
   );
 }
 
+
 export default App;
+  
